c54f7b4ef6da713ef25f03dba5eac7fc5e5772c6
ckim812 <109961170+ckim812@users.noreply.github.com>
2022-08-08 22:04:54 -0500

    updated javascript

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var topics = "HTML, CSS, Git, Javascript";
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
 console.log(topics);
 
 var topic = "HTML";
@@ -23,3 +23,30 @@ console.log(shapes[3]);
 for(var x = 0; x < shapes.length; x++) {
     console.log(shapes[x])
 }
+
+function listTopics() {
+    for (var x = 0; x < topics.length; x++) {
+        console.log(topics[x])
+    }
+}
+
+listTopics()
+
+function selectTopic() {
+    if (topics === 'HTML') {
+        console.log ("Let's study HTML!");
+    } else if (topics ==='CSS') {
+        console.log ("Let's study CSS!");
+    } else if (topics ==='Git') {
+        console.log ("Let's study Git!");
+    } else if (topics ==='JavaScript') {
+        console.log ("Let's study JavaScript!");
+    } else {
+        console.log ('Please try again!');
+    }
+}
+
+selectTopic()
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
